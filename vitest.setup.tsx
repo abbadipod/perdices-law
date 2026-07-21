eabd8fe5f3544605385760352b2d9221af4401a7
Development User <derbzz01@gmail.com>
2026-07-21 22:31:38 +0800

    Scaffold Next.js + TypeScript + Tailwind + Vitest project

---
+++ vitest.setup.tsx
@@ -0,0 +1,30 @@
+import { afterEach, vi } from "vitest";
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+
+afterEach(() => {
+  cleanup();
+});
+
+class IntersectionObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+  takeRecords() {
+    return [];
+  }
+}
+
+Object.defineProperty(window, "IntersectionObserver", {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserverMock,
+});
+
+vi.mock("next/image", () => ({
+  __esModule: true,
+  default: (props: React.ImgHTMLAttributes<HTMLImageElement>) => {
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img {...props} alt={props.alt ?? ""} />;
+  },
+}));
